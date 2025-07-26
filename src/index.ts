58519effe40027cdfb65a6f8106c669fe57f0082
KodingNYoung <abiodunadebambo44@gmail.com>
2025-07-26 15:28:57 +0100

    ci(): removed unnecssary logs

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ const TAILWIND_URL = 'https://unpkg.com/@tailwindcss/browser@4'
 const ICOMOON_URL =
   'https://cdn.jsdelivr.net/gh/eusate/eusate-chatbot-core@latest/src/assets/icomoon/style.css'
 const CHAT_URL = 'https://eusate-chatbot-core.vercel.app'
-// const CHAT_URL = 'http://localhost:3000'
 
 class ChatbotUI {
   private static container: HTMLDivElement
